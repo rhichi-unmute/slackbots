ad0537fdf2f522d78cf103b248749f1e6dbb6bf0
rhichi-unmute <73957844+rhichi-unmute@users.noreply.github.com>
2020-11-04 13:10:57 -0500

    Update closingbell.php

--- closingbell.php
+++ closingbell.php
@@ -1,5 +1,14 @@
-$user_agent = "Closing Bell (https://github.com/rhichi-unmute/slackbots/closingbell; rhichi@unmute.today)";
 $command = $_POST['command'];
 $therabuddy = $_POST['text'];
+$token = $_POST['token'];
+
+if($token != 'BPAgtuWlF4WZNr0sp8WYstx7')
+  $msg = "The token for the slash command doesn't match. Check your script.";
+  die($msg);
+  echo $msg;
+
+$user_agent = "Closing Bell (https://github.com/rhichi-unmute/slackbots/closingbell; rhichi@unmute.today)";
 
 $reply = "We just successfully closed another case! Congratulations " .$therabuddy ", and thank you for another job well done! :party-parrot:"
+
+echo $reply
